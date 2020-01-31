e8edd1a694df0755700fad854afaf3a9ca4dbe62
Fadhilah Metra <fadhilahmetra@gmail.com>
2020-01-31 16:45:12 +0700

    added further styling

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ window.addEventListener("load", function() {
 
 // opponent's choice
 let opponentMovesList = ["rock", "paper", "scissors"];
-// let opponentChoice = opponentMovesList[Math.floor(Math.random() * 3)];
-let opponentChoice = opponentMovesList[0];
+let opponentChoice = opponentMovesList[Math.floor(Math.random() * 3)];
 
 // select one of the cards
 let playerChoice = null;
@@ -21,24 +20,39 @@ let enemyHealth = document.getElementById("enemy-health-bar");
 
 let playerRock = document.getElementById("player-rock");
 playerRock.addEventListener("click", function() {
+  opponentHandReturn();
   playerChoice = "rock";
   result = winorlose(playerChoice, opponentChoice);
   healthCalc(result);
+  hurt();
+  versus();
   winCon();
+  opponentHand();
+  randomOpp();
 });
 let playerPaper = document.getElementById("player-paper");
 playerPaper.addEventListener("click", function() {
+  opponentHandReturn();
   playerChoice = "paper";
   result = winorlose(playerChoice, opponentChoice);
   healthCalc(result);
+  hurt();
+  versus();
   winCon();
+  opponentHand();
+  randomOpp();
 });
 let playerScissors = document.getElementById("player-scissors");
 playerScissors.addEventListener("click", function() {
+  opponentHandReturn();
   playerChoice = "scissors";
   result = winorlose(playerChoice, opponentChoice);
   healthCalc(result);
+  hurt();
+  versus();
   winCon();
+  opponentHand();
+  randomOpp();
 });
 
 // logic to determine who's going to win
@@ -105,9 +119,9 @@ function healthCalc(result) {
 // function too see win or lose condition
 function winCon() {
   if (playerHealth.innerHTML == 0) {
-    alert("YOU WIN!");
+    alert("YOU LOSE!");
   } else if (enemyHealth.innerHTML == 0) {
-    alert("YOU LOSE");
+    alert("YOU WIN!");
   }
 }
 
@@ -115,11 +129,67 @@ function winCon() {
 function versus() {
   switch (result) {
     case "win":
-        versusScreen.innerHTML = "YOU WON THE FIGHT!"
+      versusScreen.innerHTML = "YOU WON THE FIGHT!";
       break;
     case "lose":
+      versusScreen.innerHTML = "YOU LOST THE FIGHT!";
       break;
     case "draw":
+      versusScreen.innerHTML = "IT'S A DRAW!";
+      break;
+  }
+}
+
+// function to randomize opponent's move
+function randomOpp() {
+  opponentChoice = opponentMovesList[Math.floor(Math.random() * 3)];
+}
+
+// function to highlight what the opponent has chosen
+let enemyRock = document.getElementById("enemy-rock");
+let enemyPaper = document.getElementById("enemy-paper");
+let enemyScissors = document.getElementById("enemy-scissors");
+
+function opponentHand() {
+  switch (opponentChoice) {
+    case "rock":
+      enemyRock.style.transform = "scale(1.2)";
+      break;
+    case "paper":
+      enemyPaper.style.transform = "scale(1.2)";
+      break;
+    case "scissors":
+      enemyScissors.style.transform = "scale(1.2)";
+      break;
+  }
+}
+
+// function to turn back the size of opponent's hand to normal
+function opponentHandReturn() {
+  enemyRock.style.transform = "scale(1)";
+  enemyPaper.style.transform = "scale(1)";
+  enemyScissors.style.transform = "scale(1)";
+}
+
+// function to show hit effect
+let enemyHit = document.getElementById("enemy-hit");
+let playerHit = document.getElementById("player-hit");
+enemyHit.style.visibility = "hidden";
+playerHit.style.visibility = "hidden";
+
+function hurt() {
+  switch (result) {
+    case "win":
+      enemyHit.style.visibility = "visible";
+      setTimeout(function() {
+        enemyHit.style.visibility = "hidden";
+      }, 250);
+      break;
+    case "lose":
+      playerHit.style.visibility = "visible";
+      setTimeout(function() {
+        playerHit.style.visibility = "hidden";
+      }, 250);
       break;
   }
 }
